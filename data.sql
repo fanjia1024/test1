07cc674a3a939d82c79d5b5b4994d2a356fd9953
fanjia1024 <33594643+fanjia1024@users.noreply.github.com>
2019-10-10 10:15:02 +0800

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,9 @@
+INSERT INTO product_category (category_id, category_name, category_type, create_time, update_time) VALUES (1, '热榜', 1, '2017-03-28 16:40:22', '2019-04-15 08:38:15');
+INSERT INTO product_category (category_id, category_name, category_type, create_time, update_time) VALUES (2, '好吃的', 2, '2017-03-14 17:38:46', '2019-04-15 08:38:15');
+
+
+
+
+INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, category_type, create_time, update_time) VALUES ('157875196366160022', '皮蛋粥', 0.01, 29, '好吃的皮蛋粥', '//fuss10.elemecdn.com/0/49/65d10ef215d3c770ebb2b5ea962a7jpeg.jpeg', 0, 1, '2017-03-28 19:39:15', '2017-07-02 11:45:44');
+INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, category_type, create_time, update_time) VALUES ('157875227953464068', '慕斯蛋糕', 10.90, 200, '美味爽口', '//fuss10.elemecdn.com/9/93/91994e8456818dfe7b0bd95f10a50jpeg.jpeg', 1, 1, '2017-03-28 19:35:54', '2017-04-21 10:05:57');
+INSERT INTO product_info (product_id, product_name, product_price, product_stock, product_description, product_icon, product_status, category_type, create_time, update_time) VALUES ('164103465734242707', '蜜汁鸡翅', 0.02, 979, '好吃', '//fuss10.elemecdn.com/7/4a/f307f56216b03f067155aec8b124ejpeg.jpeg', 0, 1, '2017-03-30 17:11:56', '2017-06-24 19:20:54');
